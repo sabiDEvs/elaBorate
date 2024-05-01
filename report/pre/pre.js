5064c943d817c45040f0818b93b02723e0f8325c
Ogbaudu Oghenemaro <maroprosper14@gmail.com>
2024-05-01 23:23:02 +0100

    completed mobisite and desktop site for landing page

--- report/pre/pre.js
+++ report/pre/pre.js
@@ -1,18 +1,42 @@
+"use strict";
 (self["webpackChunkelaborate"] = self["webpackChunkelaborate"] || []).push([["report/pre/pre"],{
+
+/***/ "./src/CSS/pre.css":
+/*!*************************!*\
+  !*** ./src/CSS/pre.css ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
 
 /***/ "./src/SCRIPTS/pre.js":
 /*!****************************!*\
   !*** ./src/SCRIPTS/pre.js ***!
   \****************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _CSS_pre_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../CSS/pre.css */ "./src/CSS/pre.css");
+/* harmony import */ var _modules_header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/header */ "./src/SCRIPTS/modules/header.js");
+/* harmony import */ var _modules_footer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/footer */ "./src/SCRIPTS/modules/footer.js");
 
 
+
+const body = document.querySelector('body');
+(0,_modules_header__WEBPACK_IMPORTED_MODULE_1__.renderHeader)(body);
+(0,_modules_footer__WEBPACK_IMPORTED_MODULE_2__.renderFooter)(body);
 
 /***/ })
 
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./src/SCRIPTS/pre.js"));
+/******/ __webpack_require__.O(0, ["src_SCRIPTS_modules_footer_js-src_SCRIPTS_modules_header_js"], () => (__webpack_exec__("./src/SCRIPTS/pre.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
 /******/ }
 ]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmVwb3J0L3ByZS9wcmUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBOzs7Ozs7Ozs7Ozs7Ozs7QUNBd0I7QUFDc0I7QUFDQTtBQUM5QztBQUNBLDZEQUFZO0FBQ1osNkRBQVkiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lbGFib3JhdGUvLi9zcmMvQ1NTL3ByZS5jc3M/ZTdjOCIsIndlYnBhY2s6Ly9lbGFib3JhdGUvLi9zcmMvU0NSSVBUUy9wcmUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307IiwiaW1wb3J0ICcuLi9DU1MvcHJlLmNzcyc7XG5pbXBvcnQge3JlbmRlckhlYWRlcn0gZnJvbSAnLi9tb2R1bGVzL2hlYWRlcic7XG5pbXBvcnQge3JlbmRlckZvb3Rlcn0gZnJvbSAnLi9tb2R1bGVzL2Zvb3Rlcic7XG5jb25zdCBib2R5ID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignYm9keScpO1xucmVuZGVySGVhZGVyKGJvZHkpO1xucmVuZGVyRm9vdGVyKGJvZHkpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
