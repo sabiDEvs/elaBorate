615ca063a446f12c0dcbd4a94f0d4e95e0b6442c
Oghenemaro Ogbaudu <maroprosperofficial@gmail.com>
2025-01-27 20:19:47 +0000

    v0.0.1

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-const { merge } = require('webpack-merge');
-const dist = require('./dist.config.js');
-const sub = require('./sub.config.js');
-const report = require('./report.config.js');
-module.exports = merge(report ,sub, dist);
